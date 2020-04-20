5e0d5f8c3a6f4b105a107bbc1db4afb05467aa51
hangengsheng <1027577992@qq.com>
2020-04-20 22:35:11 +0800

    log列表

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -6,7 +6,7 @@ use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Middleware;
-use Oyhdd\Admin\Middleware\AuthMiddleware;
+use Oyhdd\Admin\Model\AdminOperationLog;
 
 /**
  * @Controller(prefix="admin/logs")
@@ -20,6 +20,7 @@ class LogController extends AdminController
      */
     public function index()
     {
-        return $this->render('logs.index');
+        $logs = AdminOperationLog::all();
+        return $this->render('logs.index', ['logs']);
     }
 }
